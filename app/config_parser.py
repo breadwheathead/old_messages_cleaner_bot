d49a3dac303b70114d601bb17205c1d84e59937c
Bread <breadwheatbox@gmail.com>
2022-12-02 10:39:42 +0700

    подготовка к переходу на другую библиотеку telethon

--- app/config_parser.py
+++ app/config_parser.py
@@ -4,9 +4,9 @@ from dataclasses import dataclass
 
 @dataclass
 class TgBot:
-    bot_token: str
-    admin_id: int
-    group_id: int
+    api_id: int
+    api_hash: str
+    username: str
 
 
 @dataclass
@@ -20,8 +20,8 @@ def load_config(path: str) -> Config:
     tg_bot = config['tg_bot']
     return Config(
         tg_bot=TgBot(
-            bot_token=tg_bot['bot_token'],
-            admin_id=int(tg_bot['admin_id']),
-            group_id=int(tg_bot['group_id'])
+            api_id=int(tg_bot['api_id']),
+            api_hash=tg_bot['api_hash'],
+            username=tg_bot['username']
         )
     )
--- bot_init.py
+++ bot_init.py
@@ -1,7 +1,3 @@
-from aiogram import Bot
-from aiogram.dispatcher import Dispatcher
 from app.config_parser import load_config
 
 config = load_config('config/bot.ini')
-bot = Bot(config.tg_bot.bot_token)
-dp = Dispatcher(bot)
